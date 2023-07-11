d217c2fda63713664c9c7465693339364ac3cdd1
Daniel Arrieta <darrieta525@gmail.com>
2023-07-10 19:20:10 -0600

    decrypter, encrypter and save and list notes

--- encrypter.py
+++ encrypter.py
@@ -1,7 +1,14 @@
 import rsa
 
-class Encrypter:
+class Encrypter():
     public_key, private_key = rsa.newkeys(512)
+
+    def __init__(self):
+        encrypter_type = "bytes"
 
     def encrypt(self, message):
         return rsa.encrypt(message.encode(), self.public_key)
+
+    def decrypt(self, bytes):
+        return rsa.decrypt(bytes, self.private_key).decode()
+    
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import rsa
+import psycopg2
 
 import note
 import encrypter
@@ -6,12 +7,34 @@ public_key, private_key = rsa.newkeys(512)
 
 body = "This is a test text to be encrypted."
 
+conn = psycopg2.connect(database="postgres",
+                        host="localhost",
+                        user='postgres',
+                        password='your_password',
+                        port='5432')
+
+def list_notes():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM notes;")
+    print(cursor.fetchall())
+
+def save_note(n:note.Note):
+    cursor = conn.cursor()
+    cursor.execute("INSERT into notes (body, author) VALUES (%s, %s)", (n.text, n.author))
+    conn.commit()
+
+
+def close_db():
+    conn.close()
+
 
 def main():
     print("Creating a new note")
     new_note = note.Note(body)
-    print(new_note)
-    print(encrypter.Encrypter.encrypt(new_note.text))
+    five_encrypter = encrypter.Encrypter()
+    new_note.text = five_encrypter.encrypt(new_note.text)
+    save_note(new_note)
+    list_notes()
 
 main()
-
+close_db()
